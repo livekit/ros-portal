bc58f08673b46b228183abb95dab1a8e9cac06d5
Alan George <alan.george@livekit.io>
2026-06-04 13:46:38 -0600

    Maybe fix

--- src/ros2_livekit_bridge/launch/livekit_bridge_local.launch.py
+++ src/ros2_livekit_bridge/launch/livekit_bridge_local.launch.py
@@ -14,8 +14,8 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-import re
 from pathlib import Path
+import re
 import subprocess
 
 from launch import LaunchDescription

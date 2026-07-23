4da76db26c53727a1433e1e4874e4d35f9add2fa
Stephen DeRosa <stephen.derosa@livekit.io>
2026-07-23 11:09:10 -0600

    python dep

--- src/test/waveshare_driver/setup.py
+++ src/test/waveshare_driver/setup.py
@@ -25,7 +25,7 @@ setup(
         ('share/ament_index/resource_index/packages', ['resource/' + package_name]),
         ('share/' + package_name, ['package.xml']),
     ],
-    install_requires=['setuptools'],
+    install_requires=['setuptools', 'python3-serial'],
     zip_safe=True,
     maintainer='Stephen DeRosa',
     maintainer_email='stephen.derosa@livekit.io',

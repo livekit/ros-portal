b52e91362fd1a84893f639178f15cd3594fcd782
stavied <40528896+stephen-derosa@users.noreply.github.com>
2026-08-05 21:39:03 -0600

    Diagnostics: build_info and node diags (#66)
    * Diagnostics: client-sdk-cpp and ros_portal version

---
+++ src/ros_portal/src/diagnostics/build_info.cpp
@@ -0,0 +1,65 @@
+/*
+ * Copyright 2026 LiveKit
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+#include "ros_portal/diagnostics/build_info.hpp"
+
+#include <cstdlib>
+#include <diagnostic_msgs/msg/diagnostic_status.hpp>
+#include <stdexcept>
+#include <utility>
+
+#include "ros_portal/version.hpp"
+
+namespace ros_portal::diagnostics {
+
+namespace {
+
+constexpr char kBuildInfoTaskName[] = "build_info";
+constexpr char kUnknownValue[] = "unknown";
+
+std::string valueOrUnknown(const char* value) { return (value != nullptr && value[0] != '\0') ? value : kUnknownValue; }
+
+} // namespace
+
+BuildInfo collectBuildInfo() {
+  BuildInfo info;
+  info.livekit_sdk_version = valueOrUnknown(ROS_PORTAL_LIVEKIT_SDK_VERSION);
+  info.ros_portal_version = valueOrUnknown(ROS_PORTAL_VERSION);
+  info.ros_distro = valueOrUnknown(std::getenv("ROS_DISTRO"));
+  return info;
+}
+
+void populateBuildInfoStatus(const BuildInfo& info, diagnostic_updater::DiagnosticStatusWrapper& status) {
+  status.summary(diagnostic_msgs::msg::DiagnosticStatus::OK, "LiveKit SDK " + info.livekit_sdk_version);
+  status.add("livekit_sdk_version", info.livekit_sdk_version);
+  status.add("ros_portal_version", info.ros_portal_version);
+  status.add("ros_distro", info.ros_distro);
+}
+
+BuildInfoDiagnostics::BuildInfoDiagnostics(DiagnosticsManagerFns diagnostics)
+    : info_(collectBuildInfo()), diagnostics_(std::move(diagnostics)) {
+  if (!diagnostics_.add || !diagnostics_.remove) {
+    throw std::invalid_argument("BuildInfoDiagnostics requires fully populated DiagnosticsManagerFns");
+  }
+
+  diagnostics_.add(kBuildInfoTaskName, [this](diagnostic_updater::DiagnosticStatusWrapper& status) {
+    populateBuildInfoStatus(info_, status);
+  });
+}
+
+BuildInfoDiagnostics::~BuildInfoDiagnostics() { diagnostics_.remove(kBuildInfoTaskName); }
+
+} // namespace ros_portal::diagnostics
---
+++ src/ros_portal/test/unit/build_info_diagnostics_test.cpp
@@ -0,0 +1,83 @@
+/*
+ * Copyright 2026 LiveKit
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+#include <gtest/gtest.h>
+
+#include <diagnostic_msgs/msg/diagnostic_status.hpp>
+#include <diagnostic_updater/diagnostic_status_wrapper.hpp>
+#include <memory>
+#include <optional>
+#include <rclcpp/rclcpp.hpp>
+#include <stdexcept>
+#include <string>
+
+#include "diagnostics_test_utils.hpp"
+#include "ros_portal/diagnostics/build_info.hpp"
+#include "ros_portal/diagnostics/diagnostics_fns.hpp"
+
+namespace ros_portal::diagnostics {
+namespace {
+
+std::optional<std::string> valueFor(const diagnostic_updater::DiagnosticStatusWrapper& status, const std::string& key) {
+  for (const auto& value : status.values) {
+    if (value.key == key) {
+      return value.value;
+    }
+  }
+  return std::nullopt;
+}
+
+TEST(BuildInfoDiagnosticsTest, PopulatesOkStatusWithVersionFields) {
+  BuildInfo info;
+  info.livekit_sdk_version = "1.5.0";
+  info.ros_portal_version = "0.1.0";
+  info.ros_distro = "humble";
+  diagnostic_updater::DiagnosticStatusWrapper status;
+
+  populateBuildInfoStatus(info, status);
+
+  EXPECT_EQ(status.level, diagnostic_msgs::msg::DiagnosticStatus::OK);
+  EXPECT_EQ(status.message, "LiveKit SDK 1.5.0");
+  EXPECT_EQ(valueFor(status, "livekit_sdk_version"), "1.5.0");
+  EXPECT_EQ(valueFor(status, "ros_portal_version"), "0.1.0");
+  EXPECT_EQ(valueFor(status, "ros_distro"), "humble");
+}
+
+TEST(BuildInfoDiagnosticsTest, CollectedInfoHasNoEmptyFields) {
+  const auto info = collectBuildInfo();
+
+  EXPECT_FALSE(info.livekit_sdk_version.empty());
+  EXPECT_FALSE(info.ros_portal_version.empty());
+  EXPECT_FALSE(info.ros_distro.empty());
+}
+
+TEST(BuildInfoDiagnosticsTest, RegistersAndRemovesTaskWithSharedHub) {
+  auto node = std::make_shared<rclcpp::Node>("build_info_diagnostics_unit_test");
+  const auto diagnostics_updater = std::make_shared<diagnostic_updater::Updater>(node);
+  diagnostics_updater->setHardwareID("ros_portal");
+  const auto fns = test::makeDiagnosticsFns(diagnostics_updater);
+
+  {
+    BuildInfoDiagnostics build_info(fns);
+  }
+}
+
+TEST(BuildInfoDiagnosticsTest, RejectsMissingDiagnostics) {
+  EXPECT_THROW(BuildInfoDiagnostics({}), std::invalid_argument);
+}
+
+} // namespace
+} // namespace ros_portal::diagnostics
